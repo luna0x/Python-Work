f0f58dcaa90e5dbda217229bb778afc5b3815a6b
luna0x <115911282+luna0x@users.noreply.github.com>
2022-10-25 14:00:40 -0600

    Update RPSgame.py

--- RPSgame.py
+++ RPSgame.py
@@ -8,7 +8,7 @@ wins = 0
 losses = 0
 ties = 0
 
-while True:  # the main game loop >>
+while True:  # the main game loop 
     print('%s WINS, %s, LOSSES %s TIES' % (wins, losses, ties))
     while True:  # player input loop
         print('Enter your move: (r)ock, (p)aper, (s)cissors or (q)uit')
